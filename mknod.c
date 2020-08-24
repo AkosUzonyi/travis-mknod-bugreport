994622a2e4d62a79a907ff49b388e1bec7574178
Uzonyi Ákos <uzonyi.akos@gmail.com>
2020-08-24 21:32:20 +0200

    printrc

--- mknod.c
+++ mknod.c
@@ -11,7 +11,7 @@ int main(void)
 {
         errno = 0;
 
-	syscall(__NR_mknod, "mknod.c", S_IFCHR | 0777, (unsigned long) 0xdeadbeef00000000ULL | makedev(1, 7));
+	printf("%ld\n", syscall(__NR_mknod, "mknod.c", S_IFCHR | 0777, (unsigned long) 0xdeadbeef00000000ULL | makedev(1, 7)));
 
         if (errno != EEXIST) {
                 perror("mknod");
