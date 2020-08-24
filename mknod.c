5d918353a3b3379e40075d5edba88cd50d4d06c4
Uzonyi Ákos <uzonyi.akos@gmail.com>
2020-08-24 21:27:54 +0200

    sane dev

--- mknod.c
+++ mknod.c
@@ -7,19 +7,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-static long
-call_mknod(unsigned short mode, unsigned long dev)
-{
-	unsigned long lmode = (unsigned long) 0xffffffffffff0000ULL | mode;
-	return syscall(__NR_mknod, "mknod.c", mode, dev);
-}
-
 int main(void)
 {
         errno = 0;
 
-	unsigned long dev = (unsigned long) 0xdeadbeef00000000ULL | makedev(1, 7);
-	call_mknod(S_IFCHR | 024, dev);
+	unsigned long dev = makedev(1, 7);
+
+
+        syscall(__NR_mknod, "mknod.c", S_IFCHR | 024, dev);
 
         if (errno != EEXIST) {
                 perror("mknod");
