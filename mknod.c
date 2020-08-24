f9f342f5ab9f70ccde311a557e88d5f91c491ec4
Uzonyi Ákos <uzonyi.akos@gmail.com>
2020-08-24 21:24:36 +0200

    copy call_mknod

--- mknod.c
+++ mknod.c
@@ -2,14 +2,25 @@
 #include <stdio.h>
 #include <sys/sysmacros.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+static long
+call_mknod(unsigned short mode, unsigned long dev)
+{
+	unsigned long lmode = (unsigned long) 0xffffffffffff0000ULL | mode;
+	return syscall(__NR_mknod, "mknod.c", lmode, dev);
+}
+
 int main(void)
 {
         errno = 0;
-        mknod("mknod.c", S_IFCHR | 024, (unsigned long) 0xdeadbeef00000000ULL | makedev(1, 7));
+
+	unsigned long dev = (unsigned long) 0xdeadbeef00000000ULL | makedev(1, 7);
+	call_mknod(S_IFCHR | 024, dev);
+
         if (errno != EEXIST) {
                 perror("mknod");
                 exit(1);
